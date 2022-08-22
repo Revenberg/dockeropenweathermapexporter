93fbd9a0cc49421ad6e9833939112e55298cf961
Sander Revenberg <sander@revenberg.info>
2022-08-22 17:43:02 +0200

    .

--- files/app/openweathermap-export.py
+++ files/app/openweathermap-export.py
@@ -1,145 +1,62 @@
-#!/usr/bin/env python3
-"""openweathermap-export"""
-from pyowm import OWM
-from pyowm.utils import config
-from pyowm.utils import timestamps
+"""Application exporter"""
 
-import logging
-import json
-import sys
 import os
 import time
-from datetime import datetime, timedelta
-from prometheus_client import Counter, Gauge, start_http_server
+from prometheus_client import start_http_server, Gauge, Enum
+import requests
 
-LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
+class AppMetrics:
+    """
+    Representation of Prometheus metrics and loop to fetch and transform
+    application metrics into Prometheus metrics.
+    """
 
-PROMETHEUS_PREFIX = os.getenv("PROMETHEUS_PREFIX", "openweathermap")
-PROMETHEUS_PORT   = int(os.getenv("PROMETHEUS_PORT", "9003"))
-PROMETHEUS_LABEL  = os.getenv("PROMETHEUS_LABEL", "openweathermap")
+    def __init__(self, app_port=80, polling_interval_seconds=5):
+        self.app_port = app_port
+        self.polling_interval_seconds = polling_interval_seconds
 
-APIKEY           = os.getenv("APIKEY", "")
-WEATHER_COUNTRY  = os.getenv("WEATHER_COUNTRY", "Surhuisterveen,NL")
-WEATHER_LANGUAGE = os.getenv("WEATHER_LANGUAGE", "NL")
+        # Prometheus metrics to collect
+        self.current_requests = Gauge("app_requests_current", "Current requests")
+        self.pending_requests = Gauge("app_requests_pending", "Pending requests")
+        self.total_uptime = Gauge("app_uptime", "Uptime")
+        self.health = Enum("app_health", "Health", states=["healthy", "unhealthy"])
 
-LOGFORMAT = '%(asctime)-15s %(message)s'
+    def run_metrics_loop(self):
+        """Metrics fetching loop"""
 
-logging.basicConfig(level=LOG_LEVEL, format=LOGFORMAT)
-LOG = logging.getLogger("openweathermap-export")
+        while True:
+            self.fetch()
+            time.sleep(self.polling_interval_seconds)
 
-# global variable
-prom_metrics = {}  # pylint: disable=C0103
-prom_msg_counter = Counter(
-    f"{PROMETHEUS_PREFIX}message_total", "Counter of received messages", [PROMETHEUS_LABEL]
-)
+    def fetch(self):
+        """
+        Get metrics from application and refresh Prometheus metrics with
+        new values.
+        """
 
-pool_frequency = int(os.getenv("POOL_FREQUENCY", "300"))
+        # Fetch raw status data from the application
+        #resp = requests.get(url=f"http://localhost:{self.app_port}/status")
+        #status_data = resp.json()
 
-config_dict = config.get_default_config()
-config_dict['language'] = WEATHER_LANGUAGE
-
-def getData(config_dict):
-    owm = OWM(APIKEY, config_dict)
-    mgr = owm.weather_manager()
-
-    # Here put your city and Country ISO 3166 country codes
-    observation = mgr.weather_at_place(WEATHER_COUNTRY)
-
-    w = observation.weather
-    # Weather details from INTERNET
-
-    prom_metric_name=PROMETHEUS_PREFIX
-    if not prom_metrics.get(prom_metric_name):
-        prom_metrics[prom_metric_name] = Gauge(
-            prom_metric_name, "metric generated from openWeather message.", [PROMETHEUS_LABEL]
-        )
-        LOG.info("creating prometheus metric: %s", prom_metric_name)
-
-    # increment received message counter
-    prom_msg_counter.labels(**{PROMETHEUS_PREFIX: PROMETHEUS_LABEL}).inc()
-
-    # expose the metric to prometheus
-    values = dict()
-
-    prom_metric_name = "Weather_" + w.status
-    if not prom_metrics.get(prom_metric_name):
-        prom_metrics[prom_metric_name] = Gauge(
-            prom_metric_name, prom_metric_name, [ prom_metric_name ], PROMETHEUS_LABEL
-        )
-        
-    # expose the metric to prometheus
-    prom_metrics[prom_metric_name].labels(**{PROMETHEUS_LABEL: prom_metric_name}).set( 1)
-    
-    
-    prom_metrics[prom_metric_name].labels(**{PROMETHEUS_LABEL: "status." + w.status}).set( 1 )
-    prom_metrics[prom_metric_name].labels(**{PROMETHEUS_LABEL: "status." + w.detailed_status}).set( 1 )
-    
-    wind  = w.wind()
-
-    prom_metrics[prom_metric_name].labels(**{PROMETHEUS_LABEL: "wind_speed" }).set( wind ["speed"] )
-    values['wind_direction_deg']  = wind ["deg"]
-    values['humidity']  = w.humidity
-
-    temperature  = w.temperature('celsius')
-    values['temp']  = temperature["temp"]
-    values['pressure'] = w.pressure['press']
-
-    values['clouds'] = w.clouds #Cloud coverage
-    values["sunrise"] = w.sunrise_time()*1000 #Sunrise time (GMT UNIXtime or ISO 8601)
-    values["sunset"] = w.sunset_time()*1000 #Sunset time (GMT UNIXtime or ISO 8601)
-    values["weather_code"] =  w.weather_code
-
-    values["weather_icon"] = w.weather_icon_name
-    values["visibility_distance"] = w.visibility_distance
-
-    location = observation.location.name
-    values["location"] = location
-
-    rain = w.rain
-    #If there is no data recorded from rain then return 0, otherwise #return the actual data
-    if len(rain) == 0:
-        values['lastrain'] = float("0")
-    else:
-        if "3h" in rain:
-            values['lastrain'] = rain["3h"]
-        if "1h" in rain:
-            values['lastrain'] = rain["1h"]
-
-    snow = w.snow
-    #If there is no data recorded from rain then return 0, otherwise #return the actual data
-    if len(snow) == 0:
-        values['lastsnow'] = float("0")
-    else:
-        if "3h" in snow:
-            values['lastsnow'] = snow["3h"]
-        if "1h" in snow:
-            values['lastsnow'] = snow["1h"]
-
-#       UV index
-    s = WEATHER_COUNTRY.split(",")
-    reg = owm.city_id_registry()
-    list_of_locations = reg.locations_for(s[0], country=s[1])
-    myLocation = list_of_locations[0]
-
-    uvimgr = owm.uvindex_manager()
-
-    uvi = uvimgr.uvindex_around_coords(myLocation.lat, myLocation.lon )
-    values['uvi'] = uvi.value
-
-    # Print the data
-    LOG.debug(values)
+        # Update Prometheus metrics with application metrics
+        self.current_requests.set(1)
+        self.pending_requests.set(2)
+        self.total_uptime.set(3)
+        self.health.state(4)
 
 def main():
-    # start prometheus server
-    start_http_server(PROMETHEUS_PORT)
+    """Main entry point"""
 
-    try:
-        while True:
-            getData(config_dict)
-            time.sleep(pool_frequency)
-    except Exception as e:
-        print(e)
-        pass
+    polling_interval_seconds = int(os.getenv("POLLING_INTERVAL_SECONDS", "300"))
+    app_port = int(os.getenv("APP_PORT", "80"))
+    exporter_port = int(os.getenv("EXPORTER_PORT", "9003"))
 
-if __name__ == '__main__':
-        main()
+    app_metrics = AppMetrics(
+        app_port=app_port,
+        polling_interval_seconds=polling_interval_seconds
+    )
+    start_http_server(exporter_port)
+    app_metrics.run_metrics_loop()
+
+if __name__ == "__main__":
+    main()
